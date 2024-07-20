ac9784c65faae30cb46e1af2ef773a5a0f1e42ad
Максим <homa19222@gmail.com>
2024-07-20 18:06:45 +0300

    Initial commit
    Аннотация @RequiredArgsConstructor из библиотеки Project Lombok используется для автоматической генерации конструктора, который принимает все final поля и поля, помеченные как @NonNull, как параметры. Это позволяет упростить создание неизменяемых объектов и упростить внедрение зависимостей.

---
+++ src/main/java/homa/clown/nextop_practicee/NextopPracticeeApplication.java
@@ -0,0 +1,13 @@
+package homa.clown.nextop_practicee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextopPracticeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextopPracticeeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/homa/clown/nextop_practicee/user/UserRepository.java
@@ -0,0 +1,12 @@
+package homa.clown.nextop_practicee.user;
+
+
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+
+}
+
