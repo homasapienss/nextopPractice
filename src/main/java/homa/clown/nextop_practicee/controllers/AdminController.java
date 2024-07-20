14faf30ded75861a9fab0057c094dcf5d1f5959c
Максим <homa19222@gmail.com>
2024-07-20 18:10:30 +0300

    commit
    контроллеры по пути которых будет тестироватся приложение

---
+++ src/main/java/homa/clown/nextop_practicee/controllers/AdminController.java
@@ -0,0 +1,27 @@
+package homa.clown.nextop_practicee.controllers;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/admin")
+@PreAuthorize("hasRole('ADMIN')")
+public class AdminController {
+    @GetMapping
+    @PreAuthorize("hasAuthority('admin:read')")
+    public String get() {
+        return "GET:: admin controller";
+    }
+    @PostMapping
+    @PreAuthorize("hasAuthority('admin:create')")
+    @Hidden
+    public String post() {
+        return "POST:: admin controller";
+    }
+}
+
+
