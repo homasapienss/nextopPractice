1377a6940e92314be21e318070dda64e37a9a83b
Максим <homa19222@gmail.com>
2024-07-20 18:16:11 +0300

    commit
    классы для запросов на регистрацию и логинизацию, и класс для ответа, ну и сервис который будет выдавать этот самый ответ по запросу

---
+++ src/main/java/homa/clown/nextop_practicee/auth/RegisterRequest.java
@@ -0,0 +1,21 @@
+package homa.clown.nextop_practicee.auth;
+
+import homa.clown.nextop_practicee.user.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private Role role;
+}
+
