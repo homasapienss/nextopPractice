d151ba725f0487b774559fea0adf8e9de29e2907
Максим <homa19222@gmail.com>
2024-07-21 17:41:04 +0300

    commit
    удаление ненужного функционала

--- src/main/java/homa/clown/nextop_practicee/user/Permission.java
+++ src/main/java/homa/clown/nextop_practicee/user/Permission.java
@@ -7,13 +7,9 @@ import lombok.RequiredArgsConstructor;
 public enum Permission {
 
     ADMIN_READ("admin:read"),
-    ADMIN_UPDATE("admin:update"),
     ADMIN_CREATE("admin:create"),
-    ADMIN_DELETE("admin:delete"),
     MANAGER_READ("management:read"),
-    MANAGER_UPDATE("management:update"),
-    MANAGER_CREATE("management:create"),
-    MANAGER_DELETE("management:delete")
+    MANAGER_CREATE("management:create")
 
     ;
 
--- src/main/java/homa/clown/nextop_practicee/user/Role.java
+++ src/main/java/homa/clown/nextop_practicee/user/Role.java
@@ -17,17 +17,11 @@ public enum Role {
     USER(Collections.emptySet()),
     ADMIN(
             Set.of(ADMIN_READ,
-                   ADMIN_UPDATE,
-                   ADMIN_DELETE,
                    ADMIN_CREATE,
                    MANAGER_READ,
-                   MANAGER_UPDATE,
-                   MANAGER_DELETE,
                    MANAGER_CREATE)),
     MANAGER(
             Set.of(MANAGER_READ,
-                   MANAGER_UPDATE,
-                   MANAGER_DELETE,
                    MANAGER_CREATE));
 
     @Getter
